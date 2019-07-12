30d44b73a922a038d5ac39cbe754991ddbb187d9
priyashR <priyash.ramawthar@gmail.com>
2019-07-12 11:48:17 +0200

    first commit

---
+++ src/main/java/com/gmail/ramawthar/priyash/budgetAppCatManRPC/BudgetAppCatManRpcApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.ramawthar.priyash.budgetAppCatManRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppCatManRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAppCatManRpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/ramawthar/priyash/budgetAppCatManRPC/BudgetAppCatManRpcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.ramawthar.priyash.budgetAppCatManRPC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudgetAppCatManRpcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
